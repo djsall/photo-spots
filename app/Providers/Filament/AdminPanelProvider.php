e30b2176ba8b513cee2f8ae60cc7752a944e524a
Levente Proksa <proksalevente@gmail.com>
2025-10-13 22:06:01 +0200

    feat: enable reg and pw reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
+            ->passwordReset()
             ->colors([
                 'primary' => Color::Cyan,
             ])
