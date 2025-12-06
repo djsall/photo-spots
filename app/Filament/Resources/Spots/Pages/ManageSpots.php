15543c38e09fe361a5b5f0f9a2409c2c302adacd
Levente Proksa <proksalevente@gmail.com>
2025-12-06 20:24:33 +0100

    feat: import action

--- app/Filament/Resources/Spots/Pages/ManageSpots.php
+++ app/Filament/Resources/Spots/Pages/ManageSpots.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Resources\Spots\Pages;
 
+use App\Enums\User\Role;
+use App\Filament\Imports\SpotImporter;
 use App\Filament\Resources\Spots\SpotResource;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ImportAction;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageSpots extends ManageRecords
@@ -14,6 +18,9 @@ class ManageSpots extends ManageRecords
     {
         return [
             CreateAction::make(),
+            ImportAction::make('import-spots')
+                ->importer(SpotImporter::class)
+                ->visible(static fn () => Filament::auth()->user()->role === Role::Admin),
         ];
     }
 }
